4170769864ac41d51366a65c540f6b4264c42f4b
Joey L <joey.iphone@me.com>
2021-06-06 21:44:26 +1000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Hi there 👋
+					<br />
+					Testing Docker and my CI Build knowledge
+					<br />
+					<br />
+					❤️ <code>Joey </code>
+				</p>
+				<a className="App-link" href="https://github.com/jo3yy">
+					My Repo
+				</a>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without crasking', () => {
+  const div = document.createElement('div');
+  render(<App />, div);
+  unmountComponentAtNode(div)
+});
+
+test('renders the paragraph', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hi there/i);
   expect(linkElement).toBeInTheDocument();
 });
